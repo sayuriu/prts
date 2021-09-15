5929a96e679636f93f264a94537e058872e28225
u/sayuri <splitpea215@gmail.com>
2021-09-16 00:18:07 +0700

    Main page: Added basic UI implementations

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -6,6 +6,58 @@ import { Component, OnInit } from '@angular/core';
 	styleUrls: ['./main.component.scss']
 })
 export class MainComponent implements OnInit {
+
+	currentMenuOptions = menuOptions;
 	constructor() {}
-	ngOnInit(): void {}
+
+	ngOnInit(): void {
+
+	}
+}
+
+const menuOptions: MenuItems[] = [
+	{
+		name: 'Combat',
+		CSSTargetAccessor: 'combat',
+		associatedBackgoundURL: 'G1_Thumbnail.png',
+		description: 'Know your enemies before fighting them.',
+		available: true,
+	},
+	{
+		name: 'Op. Info',
+		CSSTargetAccessor: 'opInfo',
+		associatedBackgoundURL: 'G3_Thumbnail.png',
+		description: 'Details of our operators.',
+		available: false,
+	},
+	{
+		name: 'Recruitment',
+		CSSTargetAccessor: 'recruit',
+		associatedBackgoundURL: 'G2_Thumbnail.png',
+		description: 'Try your luck?',
+		available: true,
+	},
+	{
+		name: 'Extra',
+		CSSTargetAccessor: 'extra',
+		associatedBackgoundURL: '',
+		description: 'Coming soon!',
+		available: false,
+	},
+	{
+		name: 'Extra',
+		CSSTargetAccessor: 'extra_2',
+		associatedBackgoundURL: '',
+		description: 'Coming soon!',
+		available: false,
+	},
+]
+
+interface MenuItems {
+	name: string;
+	CSSTargetAccessor?: string;
+	// from assets/img/menu folder
+	associatedBackgoundURL?: string;
+	description: string;
+	available: boolean;
 }
