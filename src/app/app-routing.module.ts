c45c27a606e79b742145edaded6bde427cec1740
u/sayuri <splitpea215@gmail.com>
2021-09-17 16:47:36 +0700

    Added unused routing configs / updated structure plan

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,25 +6,99 @@ import { BlankComponent } from './blank/blank.component';
 import { MainComponent as HomePage } from './main/main.component';
 
 const routes: Routes = [
-  {
-    path: '',
-    component: BlankComponent,
-    data: {
-      animation: 'DefaultPage'
-    }
-  },
-  {
-    path: 'main',
-    component: HomePage,
-    data: {
-      animation: 'HomePage'
-    }
-  },
-  {
-    path: '**',
-    pathMatch: 'full',
-    component: NotFoundComponent,
-  }
+	{
+		path: '',
+		component: BlankComponent,
+		data: {
+		animation: 'DefaultPage'
+		}
+	},
+	{
+		path: 'main',
+		component: HomePage,
+		data: {
+		animation: 'HomePage'
+		}
+	},
+	{ path: 'operators',
+		// component: OperatorsPage,
+		children: [
+			{
+				path: ':id',
+				component: BlankComponent,
+				// component: OperatorsPage,
+			},
+			{
+				path: ':name',
+				component: BlankComponent,
+				// component: OperatorsPage,
+			},
+		],
+	},
+	{ path: 'characters',
+		// component: OperatorsPage,
+		children: [
+			{
+				path: ':id',
+				component: BlankComponent,
+				// component: OperatorsPage,
+			},
+			{
+				path: ':name',
+				component: BlankComponent,
+				// component: OperatorsPage,
+			},
+		],
+	},
+	{ path: 'recruit-sim',
+		// component: RecruitPage,
+		children: [
+			{
+				path: ':id',
+				component: BlankComponent,
+				// component: OperatorsPage,
+			},
+			{
+				path: ':name',
+				component: BlankComponent,
+				// component: OperatorsPage,
+			},
+		],
+	},
+	{ path: 'combat',
+		// component: CombatPage
+		children: [
+			{
+				path: 'map',
+				component: BlankComponent,
+				// component: CombatPage:Map,
+				children: [
+					{
+						path: ':id',
+						component: BlankComponent,
+						// component: CombatPage:Map->Id,
+					}
+				]
+			},
+			{
+				path: 'enemies',
+				component: BlankComponent,
+				// component: CombatPage:Enemies,
+				children: [
+					{
+						path: ':id',
+						component: BlankComponent,
+						// component: CombatPage:Enemies->Id,
+					}
+				]
+			},
+		],
+	},
+	{
+		path: '**',
+		pathMatch: 'full',
+		component: NotFoundComponent,
+	},
 ];
 
 @NgModule({
