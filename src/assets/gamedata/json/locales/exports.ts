1e1b020094be8a5d525bae6d3f87ff38b5715d45
u/sayuri <splitpea215@gmail.com>
2021-10-30 22:51:02 +0700

    Data formatting - transfer PHASE 1: Locales [new file struct], parser and converter Aceship -> this

---
+++ src/assets/gamedata/json/locales/exports.ts
@@ -0,0 +1,13 @@
+export const Locale_Exports_PATH = {
+	base: './{locale}/',
+	chars: 'chars/{char}',
+	items: {
+		materials: 'items/inv/{item}',
+		apSupplies: 'items/apSupplies.json',
+		expItems: 'items/expItems.json',
+		itemTimeLimit: 'items/itemTimeLimit.json',
+		potentialItems: 'items/potentialItems.json',
+		uniqueInfo: 'items/uniqueInfo.json',
+	}
+}
+export default Locale_Exports_PATH;
