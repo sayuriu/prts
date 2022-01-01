400e37955110e257f2450a220caa7cf8646b38c6
u/sayuri <54628820+sayuriu@users.noreply.github.com>
2022-01-01 23:41:50 +0700

    added anims for filter menu

--- src/app/features/operators/operator-picker-area/operator-picker-area.component.ts
+++ src/app/features/operators/operator-picker-area/operator-picker-area.component.ts
@@ -1,21 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { waitSync } from '@utils/utils';
 
 @Component({
   selector: 'app-operator-picker-area',
   templateUrl: './operator-picker-area.component.html',
   styleUrls: ['./operator-picker-area.component.scss']
 })
-export class OperatorPickerAreaComponent implements OnInit {
-
+export class OperatorPickerAreaComponent implements OnInit, OnDestroy
+{
 	constructor() { }
 
+	isShown = false;
+	filterVisible = false;
 	ngOnInit(): void {
-
+		this.ToggleFilterMenu(true);
+	}
+	ngOnDestroy(): void {
+		this.ToggleFilterMenu(false);
 	}
 
-	filterVisible = true;
-	ToggleFilterMenu()
+	ToggleFilterMenu(overrideBool?: boolean)
 	{
-		this.filterVisible = !this.filterVisible;
+		this.filterVisible = overrideBool !== undefined ? overrideBool : !this.filterVisible;
+		waitSync(300);
+		this.isShown = overrideBool !== undefined ? overrideBool : !this.isShown;
 	}
 }
